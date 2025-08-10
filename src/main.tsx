16a5737382ae3a61d960624d3ab66f1288e68bef
Gecko129 <filippozanta10@gmail.com>
2025-08-10 11:03:07 +0200

    bug fixing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles/globals.css'  // Assumendo che il CSS principale si chiami style.css dentro styles/
+import './styles/globals.css'
 import App from './App'       // Import senza estensione
 
 createRoot(document.getElementById('root')!).render(
