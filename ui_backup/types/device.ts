9fce3cf4312a3e5f02fdb27bf05b467f7308a303
Gianluca Morello <gianluca.morello71@gmail.com>
2026-01-13 15:24:29 +0100

    removed the old ui backup

--- ui_backup/types/device.ts
+++
@@ -1,6 +0,0 @@
-export interface Device {
-  [x: string]: string;
-  id: string;
-  name: string;
-  // aggiungi altre proprietà che servono
-}
--- ui_backup/types/tauri.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  interface Window {
-    __TAURI__: any;
-    __TAURI_METADATA__: any;
-  }
-}
-
-export {};
